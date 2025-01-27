575d32daf0b39a702b1071f16a2c7ab2b236f2ff
Danny Davila <danny@misaki.tech>
2025-01-28 07:12:01 +0900

    [Update] LSP dependencies and LSP keymaps

--- lua/daikie/lazy/lsp.lua
+++ lua/daikie/lazy/lsp.lua
@@ -2,7 +2,55 @@ return {
   "neovim/nvim-lspconfig",
   config = function()
     local lspconfig = require("lspconfig")
-    lspconfig.lua_ls.setup {}
-    lspconfig.pyright.setup {}
+    local on_attach = require("daikie.keymaps").on_attach
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    lspconfig.lua_ls.setup {
+        settings = {
+            Lua = {
+                diagnostics = {
+                    globals = { 'vim' }
+                }
+            }
+        },
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
+    lspconfig.pyright.setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
+
+    --lspconfig.tsserver.setup {} --deprecated
+    lspconfig.ts_ls.setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
+    lspconfig.eslint.setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
+
+    lspconfig.marksman.setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
+    lspconfig.html.setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
+    lspconfig.cssls.setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
+    lspconfig.jsonls.setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
+
+    lspconfig.yamlls.setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
   end,
 }
